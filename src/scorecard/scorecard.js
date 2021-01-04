e927903f61935667b3e216a20c7459da7457a308
Michael Banvard <mjbanvard@gmail.com>
2021-01-04 15:25:16 -0600

    Cleaned scorecard.js to settle merge conflicts

--- src/scorecard/scorecard.js
+++ src/scorecard/scorecard.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './scorecard.css'
 
-<<<<<<< HEAD
 //   This is a StackOverflow solution that uses .bind and setState. -MJB
 //   class Example extends React.Component {
 //   
@@ -33,18 +32,13 @@ var handleChange = (event) => {
   this.setState({ score: event.current.value })
   // handleChange = handleChange.bind(this)
   console.log("Aren't we supposed to see this?")
-=======
-const handleChange = (event) => {
-  let keyStroke = parseInt(event.key)
-  // Trying to find where handleChange manifests
-  // console.log("Aren't we supposed to see this?")
->>>>>>> 6cc708ad448035eb87651eb155cb4c2dbdf49853
   // console.log('Status: ', this.props)
   console.log(event.key)
   if (event.key >= '0' && event.key <= '9') {
     // inScore[f].score = event.key
-    console.log('Show me ', keyStroke)
-    return keyStroke
+    // 'this.' added to keystroke, below, to eliminate IntelliSense error
+    console.log('Show me ', this.keyStroke)
+    return this.keyStroke
   }
 }
 
@@ -86,18 +80,7 @@ const score = (inScore, side) => {
             style="resize: none; width: 100%;">
             </textarea>
           </td> */}
-<<<<<<< HEAD
           <input className='inputCell' type='input' value={score[f].score} onChange={handleChange()} />
-=======
-          <input className='inputCell' type='input' 
-            defaultValue={inScore[f].score} onKeyUp={handleChange
-            /*console.log('OnKeyUp ', onKeyUp)
-            inScore[f].score.bind(onKeyUp)*/} />
-          {/* <script>
-            console.log('OnKeyUp ', onKeyUp);
-             inScore[f].score.bind(onKeyUp);
-          </script>  */}
->>>>>>> 6cc708ad448035eb87651eb155cb4c2dbdf49853
         </form>
       </div>
     )
